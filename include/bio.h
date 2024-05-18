6e110c46089048979364e2f54be43bf60d1c3bfd
Denis Burkov <hitechshell@mail.ru>
2024-05-18 19:47:53 +0500

    add forgotten function to bio.h

--- include/bio.h
+++ include/bio.h
@@ -24,4 +24,20 @@ static __inline__ unsigned readl(unsigned addr)
     return (*(volatile unsigned *) (addr));
 }
 
+static __inline__ unsigned osl_reg_get_bit(void *reg, unsigned bit_start, unsigned bit_end)
+{
+	unsigned tmp;
+	tmp = readl((unsigned long)reg);
+	return ((tmp >> bit_start)&((1 << (bit_end - bit_start + 1))-1));
+}
+
+static __inline__ void osl_reg_set_bit(void *reg, unsigned bit_start, unsigned bit_end, unsigned reg_val)
+{
+	unsigned tmp;
+	tmp = readl((unsigned long)reg);
+	tmp &= (~(((1 << (bit_end - bit_start + 1))-1) << bit_start));
+	tmp |= (reg_val << bit_start);
+	writel(tmp, (unsigned long)reg);
+}
+
 #endif	/* __OSL_BIO_H */
